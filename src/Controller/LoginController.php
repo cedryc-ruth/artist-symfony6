5d15d8e21044fe67a5c3e9999ed6f62a34eaa266
Cédryc Ruth <ceruth@epfc.eu>
2023-01-09 11:49:02 +0100

    Login-Logout 2/2

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -19,7 +19,15 @@ class LoginController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
+            'last_username' => $lastUsername,
+            'error' => $error,
         ]);
+    }
+
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout()
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
